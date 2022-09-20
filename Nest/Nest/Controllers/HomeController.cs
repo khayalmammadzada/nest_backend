db01d3f66147eb6f21fd8ba1954e0c09a1e22e49
Khayal Mammadzada <khayalmammadzada@Khayals-MacBook-Air.local>
2022-09-20 23:39:17 +0400

    frontToback

---
+++ Nest/Nest/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Nest.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+   
+}
+
